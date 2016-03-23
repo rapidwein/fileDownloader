9a578236544339af5c5ec2c56e79bbeebeefc2cc
Varun <rapidwein@gmail.com>
2016-03-22 22:53:10 -0400

    added unix FS

--- server.py
+++ server.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python
 #pyftpdlib FTP server
 
-from pyftpdlib.authorizers import DummyAuthorizer
+import logging
+import pyftpdlib.authorizers
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+from pyftpdlib.filesystems import UnixFilesystem
+
 import os
+
 def main():
   server_path = os.getcwd() + "/Server"
   try:
     os.stat(server_path)
   except:
     os.mkdir(server_path)
-  authorizer = DummyAuthorizer()
-  authorizer.add_user("user","12345",server_path, perm='elradfmwM')
-  authorizer.add_anonymous(server_path)
+  try:
+    authorizer = pyftpdlib.authorizers.UnixAuthorizer(rejected_users=["root"], require_valid_shell=True)
+    authorizer.add_user("user","12345",server_path, perm='elradfmwM')
+  except:
+    authorizer = pyftpdlib.authorizers.DummyAuthorizer()
+    authorizer.add_user("user","12345",server_path, perm='elradfmwM')
+    authorizer.add_anonymous(server_path)
   
   handler = FTPHandler
   handler.authorizer = authorizer
+  handler.abstracted_fs = UnixFilesystem
   handler.banner = "server ready"
+  logging.basicConfig(filename='/var/log/fileDownloader.log', level=logging.DEBUG)
   address = ('localhost', 2121)
   server = FTPServer(address,handler)
   #server.max_cons = 256
