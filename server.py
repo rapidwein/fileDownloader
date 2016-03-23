a03e74a4eb772b00b596667bb69ca0c770c39f66
Varun <rapidwein@gmail.com>
2016-03-22 22:59:05 -0400

    reverted server.py

--- server.py
+++ server.py
@@ -2,10 +2,9 @@
 #pyftpdlib FTP server
 
 import logging
-import pyftpdlib.authorizers
+from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
-from pyftpdlib.filesystems import UnixFilesystem
 
 import os
 
@@ -15,17 +14,12 @@ def main():
     os.stat(server_path)
   except:
     os.mkdir(server_path)
-  try:
-    authorizer = pyftpdlib.authorizers.UnixAuthorizer(rejected_users=["root"], require_valid_shell=True)
-    authorizer.add_user("user","12345",server_path, perm='elradfmwM')
-  except:
-    authorizer = pyftpdlib.authorizers.DummyAuthorizer()
-    authorizer.add_user("user","12345",server_path, perm='elradfmwM')
-    authorizer.add_anonymous(server_path)
+  authorizer = DummyAuthorizer()
+  authorizer.add_user("user","12345",server_path, perm='elradfmwM')
+  authorizer.add_anonymous(server_path)
   
   handler = FTPHandler
   handler.authorizer = authorizer
-  handler.abstracted_fs = UnixFilesystem
   handler.banner = "server ready"
   logging.basicConfig(filename='/var/log/fileDownloader.log', level=logging.DEBUG)
   address = ('localhost', 2121)
